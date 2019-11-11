70e6921569eb24c2924a7b04b1f24364e152e094
asami-hoge <56810316+asami-hoge@users.noreply.github.com>
2019-11-11 14:00:38 +0900

    -hogehoge

--- keyboards/shiro/keymaps/kadai/keymap.c
+++ keyboards/shiro/keymaps/kadai/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 T.Shinohara hogheoge
+/* Copyright 2019 T.Shinohara
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
